780aff595928ab1bc5d1be4983ffcf04fd25c11f
akfish <akfish@gmail.com>
2015-12-30 01:24:47 +0800

    Store es6 options in .babelrc

--- generators/es6/index.js
+++ generators/es6/index.js
@@ -10,22 +10,28 @@ module.exports = Fish.buildTask('es6', ["gulp-babel"],
     ignoreSrc: false,
     ignoreDst: false,
     supportSourceMap: true,
-    questions: [{
-      type: 'checkbox',
-      name: 'presets',
-      message: "Presets",
-      choices: [
-        "es2015",
-        "stage-0",
-        "stage-1",
-        "stage-2",
-        "stage-3",
-        "react"
-      ],
-      default: ["es2015"]
-    }]
+    questions: [
+      {
+        type: 'checkbox',
+        name: 'presets',
+        message: "Presets",
+        choices: [
+          "es2015",
+          "stage-0",
+          "stage-1",
+          "stage-2",
+          "stage-3",
+          "react"
+        ],
+        default: ["es2015"]
+      }
+    ]
   },
   {
+    _doWriting: function() {
+      var babelOpts = this.gulpfile.getConfig('es6', false).opts
+      this.fs.writeJSON(this.destinationPath('.babelrc'), babelOpts);
+    },
     _doInstall: function() {
       var deps = this.props.presets.map(function(s) { return "babel-preset-" + s});
       this.npmInstall(deps, { saveDev: true });
--- generators/es6/templates/task.coffee
+++ generators/es6/templates/task.coffee
@@ -1,5 +1,5 @@
 babel = heap.require('gulp-babel')
 
 gulp.task config.es6.taskName,
-  babel(config.es6.src, config.es6.dst, config.es6.opts)
+  babel(config.es6.src, config.es6.dst)
     .with(heap.sourcemaps()).if(config.es6.sourceMap)
